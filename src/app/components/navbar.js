bac8629df66217cb24bf7370eb448858ad9d6288
Cody Lawdermilt <>
2025-10-02 13:00:13 -0500

    Updated navbar

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
-    <div className='bg-red-300 p-8 text-center'>
-      <p className='font-semibold text-white'>
-        SPOTIFY TRACK PICKER
-      </p>
+    <div className="bg-white p-8 text-center flex justify-between items-center">
+      <div>
+        <p className="font-bold text-black text-2xl">SPOTIFY TRACK PICKER</p>
+      </div>
+      <div>
+        <ul className="flex gap-8 text-gray-600">
+          <li>Dashboard</li>
+          <li>Playlist</li>
+          <li>Artist</li>
+          <li>Genres</li>
+        </ul>
+      </div>
+      <div>
+        <button className="bg-purple-500 text-white p-2 rounded-md">
+          + Add Song
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
